dea7bf1d47dd37ae3693f02e32a1197e01033672
thangtv-1934 <tran.van.thang@sun-asterisk.com>
2019-06-04 14:47:30 +0700

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "rails-i18n"
 gem "bootsnap", ">= 1.1.0", require: false
 gem "bootstrap-sass", "3.3.7"
 gem "config"
+gem "bcrypt", "3.1.12"
+gem "jquery-rails"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    render file: "#{Rails.root}/public/404", layout: false, status: :not_found
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = t "wellcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
