b0bf9a7c40a6878590c4821fdbc865afdb75ec6a
thangtv-1934 <tran.van.thang@sun-asterisk.com>
2019-06-06 14:04:57 +0700

    updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    render file: "#{Rails.root}/public/404", layout: false, status: :not_found
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :load_user, except: %i(index new create)
+  before_action :admin_user, only: %i(destroy)
+
+  def index
+    @users = User.order(:name).page(params[:page]).per Settings.user.paginate_per_page
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -20,9 +25,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "users.profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.user_deleted"
+      redirect_to users_url
+    else
+      flash[:danger] = t "error"
+    end
+  end
+
   private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    render file: "#{Rails.root}/public/404", status: :not_found
+  end
 
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "users.p_login."
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
