0919e59323b3e82fee6860e1e16ea9091d2a86d8
thangtv-1934 <tran.van.thang@sun-asterisk.com>
2019-06-06 14:04:57 +0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def show
     @user = User.find_by id: params[:id]
     return if @user
@@ -12,12 +19,32 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "wellcome"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = t "check_mail"
+      redirect_to root_url
     else
       render :new
     end
+  end
+
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+
+  def update
+    @user = User.find_by id: params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = t "users.profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find_by(id: params[:id]).destroy
+    flash[:success] = t "users.user_deleted"
+    redirect_to users_url
   end
 
   private
@@ -25,4 +52,21 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
   end
+
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = t "users.p_login."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find_by id: params[:id]
+      redirect_to root_url unless current_user? @user
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
