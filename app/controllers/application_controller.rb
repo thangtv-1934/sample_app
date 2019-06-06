0e1e5c10f6c0e7ea6929051d6b9e70997ba40d9b
thangtv-1934 <tran.van.thang@sun-asterisk.com>
2019-06-06 14:04:57 +0700

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  private
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "users.p_login."
+      redirect_to login_url
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,78 @@
 class User < ApplicationRecord
-  before_save {email.downcase!}
+  has_many :microposts
+  attr_accessor :remember_token, :activation_token, :reset_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: Settings.validates.name_max}
   validates :email, presence: true, length: {maximum: Settings.validates.email_max},
     format: {with: Settings.validates.mail_regex}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.validates.password_min}
+  validates :password, presence: true, length: {minimum: Settings.validates.password_min}, allow_nil: true
+
   has_secure_password
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  def activate
+    update_attribute :activated, true
+    update_attribute :activated_at, Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest:  User.digest(reset_token)
+    update reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.expire.password.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
